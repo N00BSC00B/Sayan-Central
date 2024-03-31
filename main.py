ed0d1597dae1ba8cd8e563bf431c1ffea2fb16cb
Sayan Barma <sayanbarma2004@gmail.com>
2024-03-31 23:26:54 +0530

    updated the html pages

--- main.py
+++ main.py
@@ -2,73 +2,67 @@ from fastapi import FastAPI
 from starlette.responses import RedirectResponse, HTMLResponse
 import json
 import os
+from jinja2 import Environment, FileSystemLoader
+import tabulate
+
+with open("assets/styles.css", "r") as f:
+    css = f.read()
+
+env = Environment(loader=FileSystemLoader('assets'))
+index_template = env.get_template('index.html')
 
 app = FastAPI()
 
-
-with open("mappings.json", "r") as f:
+with open("assets/mappings.json", "r") as f:
     url_mappings = json.load(f)
-
-
-css = """
-
-"""
-
-
-def html(css, body):
-    content = f"""
-    <html>
-        <head>
-            <title>URL Forwarder</title>
-            <style>
-                {css}
-            </style>
-        </head>
-        <body>
-            {body}
-        </body>
-    </html>
-    """
-    return content
 
 
 @app.get("/")
 async def home():
     body = """
-    <h1>Welcome to the URL Forwarder service!</h1>
-    <p>Go to:</p>
-    <ul>
+    <h1>Welcome to Sayan Central!</h1>
+    <p><strong>Here are the list of my Projects:</strong></p>
+    <table>
+    <thead><tr><th>URL</th><th>Description</th></tr></thead>
+    <tbody>
     """
 
-    for short_url, long_url in url_mappings.items():
-        body += f"<li><a href='/{short_url}'>{short_url}</a> for {long_url}</li>"
-    body += """
-    </ul>
-    """
+    for key, value in url_mappings.items():
+        body += f"<tr><td><a href='/{key}'>{key}</a></td><td>{value[1]}</td></tr>"
+    body += "</tbody></table>"
 
-    return HTMLResponse(content=html(css, body))
+    content = index_template.render(
+        title="Sayan Central", body=body, css=css
+    )
+
+    return HTMLResponse(content=content)
 
 
-@app.get("/hehe")
+@app.get("/wip")
 async def hehe():
     body = """
-    <h1>Hehe</h1>
-    <p>Hehe</p>
+    <h1>Work In Progress</h1>
+    <p>This current project is under development and will be linked soon.</p>
     """
-    return HTMLResponse(content=html(css, body))
+    content = index_template.render(
+        title="Work in Progress", body=body, css=css
+    )
+    return HTMLResponse(content=content)
 
 
 @app.get("/{short_url}")
 async def redirect_to_original(short_url: str):
 
     if short_url in url_mappings:
-        return RedirectResponse(url=url_mappings[short_url])
+        return RedirectResponse(url=url_mappings[short_url][0])
     else:
         body = f"""
         <h1>URL not found</h1>
         <p>URL for <strong>{short_url}</strong> not found</p>
         """
-        content = html(css, body)
+        content = index_template.render(
+            title="URL not found", body=body, css=css
+        )
         return HTMLResponse(content=content)
 
 

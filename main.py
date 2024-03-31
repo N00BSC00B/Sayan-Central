fb66e42ef3644fde581a247aee1d78e57b95292d
Sayan Barma <sayanbarma2004@gmail.com>
2024-03-31 20:05:24 +0530

    Changes for render.com

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse, HTMLResponse
 import json
+import os
 
 app = FastAPI()
 
@@ -73,4 +74,4 @@ async def hehe():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
